a91f8cacb87786951b0e56bcfe8f9e1d64e9ad93
Hasan <nero1.hasan@hotmail.com>
2017-01-08 00:33:26 +0200

    ekleme yapıldı

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 	if($_POST){
 		$sayi1 = $_POST["sayi1"];
-		$sayi1 = $_POST["sayi2"];
+		$sayi2 = $_POST["sayi2"];
+		$islem = $_POST["islem"];
 		
-		echo $sayi1+$sayi2;
-		}
-
+		if($islem == "+"){
+			echo $sayi1+$sayi2;}
+		elseif($islem == "-"){
+			echo $sayi1-$sayi2;}
+		elseif($islem == "*"){
+			echo $sayi1*$sayi2;}
+		elseif($islem == "/"){
+			echo $sayi1/$sayi2;}
+	}
 ?>
 <form action="" method="post">
-<input type="number" name="sayi1"/><br>
-<input type="number" name="sayi2"/><br>
-<select name="">
+<input type="text" name="sayi1"/><br>
+<input type="text" name="sayi2"/><br>
+<select name="islem">
 	<option value="+">Toplama</option>
     <option value="-">Çıkartma</option>
     <option value="*">Çarpma</option>
